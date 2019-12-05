a8c53573cbe2da970535f2e39ea7acc1abea389c
inakarune <inakarune@naver.com>
2019-12-05 20:49:13 +0900

    add photo

--- src/views/Drill.vue
+++ src/views/Drill.vue
@@ -5,8 +5,48 @@
         <li></li>
         <li></li>
         <li></li>
+        <li></li>
     </ul>
 </template>
 <style lang="scss">
 @import url(../assets/css/list.scss);
+
+.product-list {
+
+    & > li:nth-child(1) {
+        background: url(../assets/img/b1-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+
+    & > li:nth-child(2) {
+        background: url(../assets/img/b2-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+
+    & > li:nth-child(3) {
+        background: url(../assets/img/b4-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+
+    & > li:nth-child(4) {
+        background: url(../assets/img/b5-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+
+    & > li:nth-child(5) {
+        background: url(../assets/img/b5-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+
+    & > li:nth-child(6) {
+        background: url(../assets/img/b7-drill.jpg);
+        background-size: cover;
+        background-position: center;
+    }
+}
 </style>
